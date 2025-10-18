1c8bdcaaadd63b8a686621cc5576489bf9965c3a
Dom Rodriguez <shymega@shymega.org.uk>
2025-10-18 14:44:25 +0100

    fix: Fix Node.js adapter in `astro.config.mjs`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 
 export default defineConfig({
   integrations: [],
   output: 'static',
-  adapter: node({
-    mode: 'standalone'
-  }),
   vite: {
     define: {
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
